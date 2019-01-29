bc8e6803400fb52cd4fe8003f2a7044e1a0ef399
Bryan Goss <bcgoss@gmail.com>
2019-01-28 20:47:53 -0800

    Adds user sign in

--- spec/system/home_page_system_spec.rb
+++ spec/system/home_page_system_spec.rb
@@ -1,11 +1,21 @@
 require 'rails_helper'
 
 describe 'Home page', js: true do
-  let!(:post) { create :post, body: 'A post!' }
+  let!(:user) { create :user, email: 'visitor@example.com', password: 'password' }
+  let!(:post) { create :post, body: 'A post!', user: user }
 
   it 'loads the feed' do
     visit '/'
     expect(page).to have_content('Broadcast')
+    expect(page).to have_content('Sign in')
+
+    click_on 'Sign in'
+    fill_in 'Email', with: 'visitor@example.com'
+    fill_in 'Password', with: 'password'
+    click_on 'Log in'
+
+    expect(current_path).to eq root_path
+    
     expect(page).to have_content('Incoming messages:') 
 
     within('.feed') do
